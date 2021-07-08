fe51ed6e6f687a6e4bc1d61b7e2ccee064c7ef11
Yeseul Lee <dev.yezlee@gmail.com>
2021-07-08 19:38:17 +0900

    Dynamic component generation - practice props

--- src/App.js
+++ src/App.js
@@ -1,43 +1,42 @@
-function Food({ foodname, photo }) {
+function Food({ foodName, photo }) {
   return (
     <div>
-      <h2>I love {foodname}</h2>
+      <h2>I love {foodName}</h2>
       <img src={photo} />
     </div>
   );
 }
 
-function App() {
-  const foodILike = [
-    {
-      name: "Kimchi",
-      image:
-        "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg",
-    },
-    {
-      name: "bulgogi",
-      image:
-        "https://www.google.com/search?q=%EB%B6%88%EA%B3%A0%EA%B8%B0&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk02ziUIXmsNvR7TuYKh1cpJGpdpH8g:1593509047151&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi-p87lm6nqAhVbUd4KHVSWBBUQ_AUoAXoECBgQAw&biw=1440&bih=701&dpr=2#imgrc=kjZoAmvHGo-KfM",
-    },
-    {
-      name: "kimbap",
-      image:
-        "https://www.google.com/search?q=%EA%B9%80%EB%B0%A5&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk03dn_nrK6M5JlKKfD5krm8w3qTmGQ:1593509069629&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjqn6rwm6nqAhXDQN4KHYLUA4EQ_AUoAXoECBgQAw&biw=1440&bih=701#imgrc=Xz3xQoYZw3bqEM",
-    },
-    {
-      name: "samgyetang",
-      image:
-        "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg",
-    },
-  ];
+const foodILike = [
+  {
+    name: "Kimchi",
+    image:
+      "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg",
+  },
+  {
+    name: "bulgogi",
+    image:
+      "https://www.google.com/search?q=%EB%B6%88%EA%B3%A0%EA%B8%B0&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk02ziUIXmsNvR7TuYKh1cpJGpdpH8g:1593509047151&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi-p87lm6nqAhVbUd4KHVSWBBUQ_AUoAXoECBgQAw&biw=1440&bih=701&dpr=2#imgrc=kjZoAmvHGo-KfM",
+  },
+  {
+    name: "kimbap",
+    image:
+      "https://www.google.com/search?q=%EA%B9%80%EB%B0%A5&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk03dn_nrK6M5JlKKfD5krm8w3qTmGQ:1593509069629&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjqn6rwm6nqAhXDQN4KHYLUA4EQ_AUoAXoECBgQAw&biw=1440&bih=701#imgrc=Xz3xQoYZw3bqEM",
+  },
+  {
+    name: "samgyetang",
+    image:
+      "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg",
+  },
+];
 
-  return (
-    <div className="App">
-      {foodILike.map((dish) => (
-        <Food foodname={dish.name} photo={dish.image}></Food>
-      ))}
-    </div>
-  );
+function renderFood(dish) {
+  console.log(dish);
+  return <Food foodName={dish.name} photo={dish.image} />;
+}
+
+function App() {
+  return <div>{foodILike.map(renderFood)}</div>;
 }
 
 export default App;
