7e83b5262f8f3e68b77dbdec7977f251847782bd
Yeseul Lee <dev.yezlee@gmail.com>
2021-07-08 19:19:48 +0900

    Dynamic component generation - practice props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+function Food({ foodname, photo }) {
+  return (
+    <div>
+      <h2>I love {foodname}</h2>
+      <img src={photo} />
+    </div>
+  );
+}
 
 function App() {
+  const foodILike = [
+    {
+      name: "Kimchi",
+      image:
+        "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg",
+    },
+    {
+      name: "bulgogi",
+      image:
+        "https://www.google.com/search?q=%EB%B6%88%EA%B3%A0%EA%B8%B0&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk02ziUIXmsNvR7TuYKh1cpJGpdpH8g:1593509047151&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi-p87lm6nqAhVbUd4KHVSWBBUQ_AUoAXoECBgQAw&biw=1440&bih=701&dpr=2#imgrc=kjZoAmvHGo-KfM",
+    },
+    {
+      name: "kimbap",
+      image:
+        "https://www.google.com/search?q=%EA%B9%80%EB%B0%A5&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk03dn_nrK6M5JlKKfD5krm8w3qTmGQ:1593509069629&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjqn6rwm6nqAhXDQN4KHYLUA4EQ_AUoAXoECBgQAw&biw=1440&bih=701#imgrc=Xz3xQoYZw3bqEM",
+    },
+    {
+      name: "samgyetang",
+      image:
+        "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg",
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {foodILike.map((dish) => (
+        <Food foodname={dish.name} photo={dish.image}></Food>
+      ))}
     </div>
   );
 }
