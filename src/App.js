1aef132f88a5fd5294c175ab3551ffac200052bb
Yeseul Lee <dev.yezlee@gmail.com>
2021-07-08 20:27:05 +0900

    propTypes

--- src/App.js
+++ src/App.js
@@ -1,42 +1,65 @@
-function Food({ foodName, photo }) {
+import propTypes from "prop-types";
+
+const foodILike = [
+  {
+    id: 1,
+    name: "Kimchi",
+    image:
+      "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg",
+    rating: 5,
+  },
+  {
+    id: 2,
+    name: "bulgogi",
+    image:
+      "https://www.google.com/search?q=%EB%B6%88%EA%B3%A0%EA%B8%B0&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk02ziUIXmsNvR7TuYKh1cpJGpdpH8g:1593509047151&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi-p87lm6nqAhVbUd4KHVSWBBUQ_AUoAXoECBgQAw&biw=1440&bih=701&dpr=2#imgrc=kjZoAmvHGo-KfM",
+    rating: 4.5,
+  },
+  {
+    id: 3,
+    name: "kimbap",
+    image:
+      "https://www.google.com/search?q=%EA%B9%80%EB%B0%A5&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk03dn_nrK6M5JlKKfD5krm8w3qTmGQ:1593509069629&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjqn6rwm6nqAhXDQN4KHYLUA4EQ_AUoAXoECBgQAw&biw=1440&bih=701#imgrc=Xz3xQoYZw3bqEM",
+    rating: 3,
+  },
+  {
+    id: 4,
+    name: "samgyetang",
+    image:
+      "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg",
+    rating: 4,
+  },
+];
+
+function Food({ foodName, photo, rating }) {
   return (
     <div>
       <h2>I love {foodName}</h2>
-      <img src={photo} />
+      <h4>{rating}/5.0</h4>
+      <img src={photo} alt={foodName} />
     </div>
   );
 }
 
-const foodILike = [
-  {
-    name: "Kimchi",
-    image:
-      "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg",
-  },
-  {
-    name: "bulgogi",
-    image:
-      "https://www.google.com/search?q=%EB%B6%88%EA%B3%A0%EA%B8%B0&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk02ziUIXmsNvR7TuYKh1cpJGpdpH8g:1593509047151&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi-p87lm6nqAhVbUd4KHVSWBBUQ_AUoAXoECBgQAw&biw=1440&bih=701&dpr=2#imgrc=kjZoAmvHGo-KfM",
-  },
-  {
-    name: "kimbap",
-    image:
-      "https://www.google.com/search?q=%EA%B9%80%EB%B0%A5&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk03dn_nrK6M5JlKKfD5krm8w3qTmGQ:1593509069629&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjqn6rwm6nqAhXDQN4KHYLUA4EQ_AUoAXoECBgQAw&biw=1440&bih=701#imgrc=Xz3xQoYZw3bqEM",
-  },
-  {
-    name: "samgyetang",
-    image:
-      "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg",
-  },
-];
-
-function renderFood(dish) {
-  console.log(dish);
-  return <Food foodName={dish.name} photo={dish.image} />;
-}
+Food.propTypes = {
+  foodName: propTypes.string.isRequired,
+  photo: propTypes.string.isRequired,
+  rating: propTypes.number,
+};
 
 function App() {
-  return <div>{foodILike.map(renderFood)}</div>;
+  return (
+    <div>
+      {foodILike.map((dish) => (
+        <Food
+          key={dish.id}
+          foodName={dish.name}
+          photo={dish.image}
+          rating={dish.rating}
+        ></Food>
+      ))}
+    </div>
+  );
 }
 
 export default App;
